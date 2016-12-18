be47cd5902b66eb71ffbf6b38d3adae72f29977a
Sabbir <ssubhan@noggin.com.au>
2016-12-19 10:31:24 +1100

    some change

--- src/test/java/com/noggin/OCA1Atumation/AWebDriverOCAFirstwithLibaryTest.java
+++ src/test/java/com/noggin/OCA1Atumation/AWebDriverOCAFirstwithLibaryTest.java
@@ -10,7 +10,6 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import com.noggin.OCA1Automation.FindOCAElement;
 
 import junit.framework.Assert;
