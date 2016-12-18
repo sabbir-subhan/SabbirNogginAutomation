96cea2c8a419124d4dc3f02f7f9d21727d1ca426
Sabbir <ssubhan@noggin.com.au>
2016-12-19 10:29:40 +1100

    more change to JSoup example file

--- src/main/java/com/noggin/OCA1Automation/JSoupExample.java
+++ src/main/java/com/noggin/OCA1Automation/JSoupExample.java
@@ -1,7 +1,11 @@
 package com.noggin.OCA1Automation;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -21,14 +25,18 @@ public class JSoupExample {
 		
 		//Jsoup to read html files and find elements of that HTML file (i.e Selenium Test Cases)
 		//Reference: https://jsoup.org/cookbook/extracting-data/dom-navigation
-		File input = new File("C://SeleniumIDETestCase//Creating User accounts.html");
-		Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+		File input = new File("C://SeleniumIDETestCase//login.html");
+		Document doc = Jsoup.parse(input, "UTF-8");
 		Elements contents = doc.getElementsByTag("td");
 		int x=1;
 		String part1="";
 		String part2="";
 		String part3="";
 		int j=1;
+		//creating an array list
+		int arrayIndex=0;
+		ArrayList<String> OutPutArray = new ArrayList<String>();
+	
 //		String[] textArray = null;
 		for(Element content: contents){
 			String Text=content.text();
@@ -50,7 +58,13 @@ public class JSoupExample {
 						 * and save in java or text file.
 						 */
 						String finalString=part1+part2+part3;
-						System.out.println(x+".Combined String:"+finalString);
+						//add to string array list
+						//arryindex
+						
+						OutPutArray.add(finalString);
+						
+						System.out.println(x+".Combined String:"+OutPutArray.get(arrayIndex));
+						arrayIndex++;
 						part1="";
 						part2="";
 						part3="";
@@ -63,7 +77,26 @@ public class JSoupExample {
 				}
 			x=x+1;
 			}
+		//Write array list file
+		savetoFile(OutPutArray);
 		}
+
+	public static void savetoFile(ArrayList<String> OutPutArray) throws FileNotFoundException {
+		try{
+		File JavaFile = new File("C://SeleniumIDETestCase//Test.java");
+		FileOutputStream FlieOutPut=new FileOutputStream(JavaFile);
+		
+		PrintWriter pw = new PrintWriter(FlieOutPut);
+	    for (int i=0;i<OutPutArray.size();i++)
+	        pw.println(OutPutArray.get(i));
+	    pw.close();
+		}
+		catch (IOException e) {
+        	System.err.println("Problem writing to the file");
+        	//.e. null;
+    }
+		
+	}
 }
 
 
