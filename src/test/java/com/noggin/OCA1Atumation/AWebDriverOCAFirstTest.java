871afc9ea35f2b93a4aac131bbca5b52529c967b
Sabbir <ssubhan@noggin.com.au>
2016-12-19 10:36:08 +1100

    remove unused import

--- src/test/java/com/noggin/OCA1Atumation/AWebDriverOCAFirstTest.java
+++ src/test/java/com/noggin/OCA1Atumation/AWebDriverOCAFirstTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.noggin.OCA1Automation.FindOCAElement;
